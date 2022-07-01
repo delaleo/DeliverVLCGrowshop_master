3f0c6e4696f14aadca2e650e32ffbd7eafa0946f
Leo <delafuente.leonardo@gmail.com>
2022-07-01 13:58:36 +0100

    fader JS

--- DeliverVLCGrowshop/js/scripts.js
+++ DeliverVLCGrowshop/js/scripts.js
@@ -5,3 +5,21 @@
  */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+window.addEventListener("scroll", function(){
+
+let element = document.getElementById("form-banc");
+let screenSize = window.innerHeight;
+
+if(element.getBoundingClientRect().top < screenSize) {
+    element.classList.add('visible');
+  } else {
+    element.classList.remove('visible');
+  }
+});
+
+
+
+
+
